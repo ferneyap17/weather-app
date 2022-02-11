661564e3627d8c2d0e57d57510ddce0dadd77e21
Hugo Arias <hugoarias@wposs.com>
2022-02-11 11:18:06 -0500

    Se maqueta el componente weather-forecast y weather-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherTodayComponent } from './components/weather-today/weather-today.component';
+import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
+import { WeatherCardComponent } from './components/weather-forecast/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherTodayComponent
+    WeatherTodayComponent,
+    WeatherForecastComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
